b4a96eea87725cdb325d0cd8e19a04271d5d8679
Jonathan Chasse <jonathan.r.chasse@gmail.com>
2020-07-08 18:02:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,10 @@ def long_planeteer_calls(array)
 end
 
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.include? do |cheese|
+    cheese == cheese_types
+    end
+  end
 end
