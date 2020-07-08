362043bcddadb9d421bfaba0b4eccf147cb50450
Jonathan Chasse <jonathan.r.chasse@gmail.com>
2020-07-08 17:57:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,16 +4,20 @@ def roll_call_dwarves(array)
   end
 end
 
+
 def summon_captain_planet(array)
   array.collect do |item|
     "#{item.capitalize}!"
   end
 end
 
-def long_planeteer_calls
 
-
+def long_planeteer_calls(array)
+  array.any? do |call|
+    call.length > 4
+  end
 end
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
